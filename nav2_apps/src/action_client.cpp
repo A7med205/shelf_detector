b2b7a91f0fd1aed99ad9189b54bee1a247ad100d
Ahmed M <ahmedhmair@gmail.com>
2024-12-09 21:29:18 +0000

    Added control_ timer to main node.

--- nav2_apps/src/action_client.cpp
+++ nav2_apps/src/action_client.cpp
@@ -1,8 +1,11 @@
+#include "geometry_msgs/msg/twist.hpp"
 #include "nav2_msgs/action/navigate_to_pose.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
 #include <std_msgs/msg/int32.hpp>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2/LinearMath/Quaternion.h>
 
 using NavigateToPose = nav2_msgs::action::NavigateToPose;
 
@@ -13,10 +16,10 @@ public:
 
   NavigateToPoseClient() : Node("navigate_to_pose_client") {
     // Initialize the action client
-    client_ =
+    act_client_ =
         rclcpp_action::create_client<NavigateToPose>(this, "/navigate_to_pose");
 
-    main_command = this->create_subscription<std_msgs::msg::Int32>(
+    command_sub = this->create_subscription<std_msgs::msg::Int32>(
         "/command_topic", 10,
         std::bind(&NavigateToPoseClient::command_callback, this,
                   std::placeholders::_1));
@@ -25,26 +28,120 @@ public:
         "/odom", 10,
         std::bind(&NavigateToPoseClient::odom_callback, this,
                   std::placeholders::_1));
+    cmd_vel_publisher =
+        this->create_publisher<geometry_msgs::msg::Twist>(vel_topic, 10);
+    controller_ =
+        this->create_wall_timer(std::chrono::milliseconds(200),
+                                std::bind(&TimerNode::control_callback, this));
 
-    // Wait for the action server
+    waypoints = {0.68, 0.04, 0.92, -2.09, 2.27, -2.04,
+                 2.49, 0.10, 4.39, 0.11,  5.46, -0.03};
+
+    // Waiting for the /navigate_to_pose action server
+    if (!act_client_->wait_for_action_server(std::chrono::seconds(10))) {
+      RCLCPP_ERROR(this->get_logger(),
+                   "Action server not available after waiting");
+      return;
+    }
   }
 
 private:
   void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) {
+    current_x = msg->pose.pose.position.x;
+    current_y = msg->pose.pose.position.y;
 
-  
-    if (command_ == 1) {
-      send_goal(0.68, 0.0, 0.0, 1.0);
-    }
+    tf2::Quaternion q(
+        msg->pose.pose.orientation.x, msg->pose.pose.orientation.y,
+        msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+    tf2::Matrix3x3 m(q);
+    double roll, pitch, yaw;
+    m.getRPY(roll, pitch, yaw);
+    yaw_ = yaw;
   }
 
   // ros2 topic pub -1 /command_topic std_msgs/Int32 "{data: 1}"
   void command_callback(const std_msgs::msg::Int32::SharedPtr msg) {
-    RCLCPP_INFO(this->get_logger(), "I received: '%d'", msg->data);
-
     if (msg->data == 1) {
+      RCLCPP_INFO(this->get_logger(), "I received: '%d'", msg->data);
       command_ = 1;
     }
+  }
+
+  void control_callback() {
+    if (command_ == 1) {
+      int end_index, goal_index;
+      next_waypoint(end_index, goal_index);
+
+      // Rotating towards the identified waypoint
+      double angle_to_waypoint =
+          std::atan2(waypoints[goal_index + 1] - current_y,
+                     waypoints[goal_index] - current_x);
+      double yaw_error = angle_to_waypoint - yaw_;
+
+      // Publishing twist message
+      geometry_msgs::msg::Twist cmd_msg;
+      cmd_msg.linear.x = 0.0;        // No forward movement, just rotation
+      cmd_msg.angular.z = yaw_error; // Rotate towards waypoint
+      cmd_vel_publisher->publish(cmd_msg);
+
+      command_ = (std::abs(yaw_error) > 0.2) ? 1 : 0;
+    }
+  }
+
+  void next_waypoint(int &end_index, int &goal_index) {
+
+    // Determining the closest end waypoint
+    double dist_to_first =
+        std::hypot(waypoints[0] - current_x, waypoints[1] - current_y);
+    double dist_to_last =
+        std::hypot(waypoints[waypoints.size() - 2] - current_x,
+                   waypoints[waypoints.size() - 1] - current_y);
+
+    double target_x = (dist_to_first < dist_to_last)
+                          ? waypoints[0]
+                          : waypoints[waypoints.size() - 2];
+    double target_y = (dist_to_first < dist_to_last)
+                          ? waypoints[1]
+                          : waypoints[waypoints.size() - 1];
+
+    // End x index
+    end_index = (dist_to_first < dist_to_last) ? 0 : waypoints.size() - 2;
+
+    // Finding the closest waypoint in the direction of the closest end
+    double closest_x = target_x;
+    double closest_y = target_y;
+    double min_distance = std::numeric_limits<double>::max();
+
+    for (size_t i = 0; i < waypoints.size(); i += 2) {
+      double wp_x = waypoints[i];
+      double wp_y = waypoints[i + 1];
+
+      double vector_to_wp_x = wp_x - current_x;
+      double vector_to_wp_y = wp_y - current_y;
+      double vector_to_target_x = target_x - current_x;
+      double vector_to_target_y = target_y - current_y;
+
+      double dot_product = vector_to_wp_x * vector_to_target_x +
+                           vector_to_wp_y * vector_to_target_y;
+      double magnitude_wp = std::hypot(vector_to_wp_x, vector_to_wp_y);
+      double magnitude_target =
+          std::hypot(vector_to_target_x, vector_to_target_y);
+      double angle = std::acos(dot_product / (magnitude_wp * magnitude_target));
+
+      if (angle <= M_PI_2) {
+        double distance = std::hypot(vector_to_wp_x, vector_to_wp_y);
+        if (distance < min_distance) {
+          min_distance = distance;
+          closest_x = wp_x;
+          closest_y = wp_y;
+
+          // Goal x index
+          goal_index = i;
+        }
+      }
+    }
+
+    return 0;
   }
 
   void send_goal(double x, double y, double oz, double ow) {
@@ -73,7 +170,7 @@ private:
         &NavigateToPoseClient::result_callback, this, std::placeholders::_1);
 
     // Send the goal
-    client_->async_send_goal(goal_msg, send_goal_options);
+    act_client_->async_send_goal(goal_msg, send_goal_options);
   }
 
   void goal_response_callback(
@@ -114,12 +211,15 @@ private:
     // rclcpp::shutdown();
   }
 
-  rclcpp_action::Client<NavigateToPose>::SharedPtr client_;
-  rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr main_command;
+  rclcpp_action::Client<NavigateToPose>::SharedPtr act_client_;
+  rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr command_sub;
   rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr odom_sub;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher;
+  rclcpp::TimerBase::SharedPtr controller_;
 
+  std::vector<double> waypoints;
+  double current_x, current_y, yaw_;
   int command_;
-  bool waiting_;
 };
 
 int main(int argc, char **argv) {
